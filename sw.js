79c7a6ce5d4c173adca8a56adc56ab698aef7b8f
Bloki3 <ollypout15@hotmail.com>
2026-02-26 17:30:11 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-self.addEventListener('install', () => console.log("Blokie Installed"));
+self.addEventListener('install', () => console.log("Service Worker Active"));
 self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
